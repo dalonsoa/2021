9e92f22b06e14071f1ce07a4206760a6fc650bcd
Diego <d.alonso-alvarez@imperial.ac.uk>
2021-12-01 07:01:12 +0000

    Complete Day 01

--- day01/bin/day01.dart
+++ day01/bin/day01.dart
@@ -1,3 +1,25 @@
+import 'dart:io';
+
+/// Find the number of increasing values using a window step.
+int increasing(List<int> values, int window) {
+  int deeper = 0;
+
+  for (int i = window; i <= values.length - 1; i++) {
+    if (values[i] > values[i - window]) {
+      deeper++;
+    }
+  }
+  return deeper;
+}
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  File file = File('data/input');
+
+  List<int> depth = file.readAsLinesSync().map(int.parse).toList();
+
+  int value = increasing(depth, 1);
+  print("Answer to Part 1: $value");
+
+  value = increasing(depth, 3);
+  print("Answer to Part 2: $value");
 }
